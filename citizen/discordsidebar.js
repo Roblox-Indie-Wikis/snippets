15896c4458138c1d58a8253649377e1579d78de9
WildVoltaireian <dostalyavoltaire@gmail.com>
2025-02-10 19:51:40 -0500

    Create discordsidebar.js

---
+++ citizen/discordsidebar.js
@@ -0,0 +1,3 @@
+// this script adds an extra button to the Citizen sidebar. Can be used for other purposes too.
+$(document).ready( function () {
+   $('.citizen-drawer').after($('<div class="citizen-discord citizen-header__item mw-logo-icon"><a href="https://discord.gg/SERVERINVITE"><img src="https://static.wikitide.net/outlasterwiki/3/3c/Discordlg.svg" width="40" height="40" alt="Discord button."></a></div>'));
